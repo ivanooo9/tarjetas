58b7109820a0aead3ea94c3b9ddef5d3d2799614
ivanooo9 <marcoeli098@gmail.com>
2025-02-27 19:51:38 -0500

    Mensaje de commit descriptivo

---
+++ lib/descripcion.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+
+class ItemDetailPage extends StatefulWidget {
+  final String name;
+  final double price;
+  final String location;
+  final double rating;
+  final String description;
+  final String owner;
+  final String email;
+  final String image;
+
+  const ItemDetailPage({
+    required this.name,
+    required this.price,
+    required this.location,
+    required this.rating,
+    required this.description,
+    required this.owner,
+    required this.email,
+    required this.image,
+  });
+
+  @override
+  _ItemDetailPageState createState() => _ItemDetailPageState();
+}
+
+class _ItemDetailPageState extends State<ItemDetailPage> {
+  String selectedCategory = 'Todos'; // Estado para la categoría seleccionada
+
+  static const List<Map<String, String>> categories = [
+    {'name': 'Fútbol', 'price': '\$20/hora', 'icon': 'sports_soccer'},
+    {'name': 'Basket', 'price': '\$15/hora', 'icon': 'sports_basketball'},
+    {'name': 'Volley', 'price': '\$18/hora', 'icon': 'sports_volleyball'},
+  ];
+
+  void _showReservation(BuildContext context, String name, String price) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Reserva realizada para $name por $price')),
+    );
+  }
+
+  // Método para filtrar las categorías según la selección
+  List<Map<String, String>> getFilteredCategories() {
+    if (selectedCategory == 'Todos') {
+      return categories;
+    } else {
+      return categories
+          .where((category) => category['name'] == selectedCategory)
+          .toList();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.name),
+        backgroundColor: Color(0xFF19382F),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Foto destacada
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.asset(
+                widget.image,
+                height: 200,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 20),
+            // Detalles del complejo deportivo
+            Text("Ubicación: ${widget.location}", style: TextStyle(fontSize: 18)),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Text("Calificación: ", style: TextStyle(fontSize: 18)),
+                Row(
+                  children: List.generate(
+                    5,
+                    (index) => Icon(
+                      index < widget.rating ? Icons.star : Icons.star_border,
+                      color: Colors.yellow,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Text("Descripción: ${widget.description}", style: TextStyle(fontSize: 18)),
+            SizedBox(height: 10),
+            Text("Propietario: ${widget.owner}", style: TextStyle(fontSize: 18)),
+            SizedBox(height: 10),
+            Text("Email: ${widget.email}", style: TextStyle(fontSize: 18)),
+            SizedBox(height: 20),
+            // Tarjetas de categorías filtradas
+            ...getFilteredCategories().map((category) => Card(
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              _getIconForCategory(category['icon']!),
+                              size: 24,
+                              color: Color(0xFF84A05A),
+                            ),
+                            SizedBox(width: 8),
+                            Text("Nombre: ${category['name']}",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+                        Text("Precio: ${category['price']}",
+                            style: TextStyle(fontSize: 18)),
+                        SizedBox(height: 8),
+                        ElevatedButton(
+                          onPressed: () => _showReservation(
+                              context, category['name']!, category['price']!),
+                          child: Text("Reservar"),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF84A05A),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ))
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // Botón para mostrar todos
+              IconButton(
+                icon: Icon(Icons.all_inclusive, color: Colors.white),
+                onPressed: () {
+                  setState(() {
+                    selectedCategory = 'Todos';
+                  });
+                },
+              ),
+              // Botón de Fútbol
+              IconButton(
+                icon: Icon(Icons.sports_soccer, color: Colors.white),
+                onPressed: () {
+                  setState(() {
+                    selectedCategory = 'Fútbol';
+                  });
+                },
+              ),
+              // Botón de Basketball
+              IconButton(
+                icon: Icon(Icons.sports_basketball, color: Colors.white),
+                onPressed: () {
+                  setState(() {
+                    selectedCategory = 'Basket';
+                  });
+                },
+              ),
+              // Botón de Volleyball
+              IconButton(
+                icon: Icon(Icons.sports_volleyball, color: Colors.white),
+                onPressed: () {
+                  setState(() {
+                    selectedCategory = 'Volley';
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+        color: Color(0xFF19382F),
+      ),
+    );
+  }
+
+  // Método para obtener el icono según la categoría
+  IconData _getIconForCategory(String icon) {
+    switch (icon) {
+      case 'sports_soccer':
+        return Icons.sports_soccer;
+      case 'sports_basketball':
+        return Icons.sports_basketball;
+      case 'sports_volleyball':
+        return Icons.sports_volleyball;
+      default:
+        return Icons.sports;
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,317 @@
+import 'package:flutter/material.dart';
+import 'descripcion.dart'; // Importa la página de detalles separada
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sports Complex',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  // Mapa de categorías con sus respectivos ítems
+  final Map<String, List<Item>> categories = {
+    "Soccer 6": [
+      Item(
+        image: 'assets/images/cancha01.jpg',
+        name: 'Soccer Field 1',
+        price: 100000,
+        location: "Location 1",
+        rating: 4.5,
+        description: "Campo de Soccer 6 con césped natural",
+        owner: "Carlos",
+        email: "carlos@example.com",
+      ),
+      Item(
+        image: 'assets/images/cancha06.jpg',
+        name: 'Soccer Field 2',
+        price: 120000,
+        location: "Location 2",
+        rating: 4.0,
+        description: "Amplio campo para partidos intensos",
+        owner: "Miguel",
+        email: "miguel@example.com",
+      ),
+    ],
+    "Soccer 9": [
+      Item(
+        image: 'assets/images/cancha03.jpg',
+        name: 'Soccer Field 3',
+        price: 150000,
+        location: "Location 3",
+        rating: 4.2,
+        description: "Cancha de Soccer 9 con luz nocturna",
+        owner: "Luis",
+        email: "luis@example.com",
+      ),
+      Item(
+        image: 'assets/images/cancha04.jpg',
+        name: 'Soccer Field 4',
+        price: 130000,
+        location: "Location 4",
+        rating: 4.3,
+        description: "Espacioso y bien mantenido",
+        owner: "Ana",
+        email: "ana@example.com",
+      ),
+    ],
+    "Volleyball": [
+      Item(
+        image: 'assets/images/cancha05.jpg',
+        name: 'Volleyball Court 1',
+        price: 80000,
+        location: "Location 5",
+        rating: 4.7,
+        description: "Cancha de Volleyball al aire libre",
+        owner: "Sofía",
+        email: "sofia@example.com",
+      ),
+    ],
+    "Basketball": [
+      Item(
+        image: 'assets/images/cancha02.jpg',
+        name: 'Basketball Court 1',
+        price: 90000,
+        location: "Location 6",
+        rating: 4.6,
+        description: "Cancha de Basketball indoor",
+        owner: "Juan",
+        email: "juan@example.com",
+      ),
+    ]
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    // Combina todos los ítems de todas las categorías en una sola lista
+    List<Item> allItems =
+        categories.values.expand((items) => items).toList();
+
+    return Scaffold(
+      backgroundColor: Color(0xFFDDE6D5),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF84A05A),
+        elevation: 0,
+        leading: Icon(Icons.menu, color: Colors.white),
+        actions: [
+          Icon(Icons.shopping_cart, color: Colors.white),
+          SizedBox(width: 20),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Sección de usuario
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Color(0xFF19382F),
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    // Asegúrate de tener la imagen en: assets/images/profile.jpg
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/profile.jpg'),
+                      radius: 30,
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Ivan Peralta",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold)),
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 8, vertical: 4),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Text("VIP",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text("Rp 980.528",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold)),
+                    Text("18 Orders",
+                        style: TextStyle(color: Colors.white70)),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          // Barra de búsqueda
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Buscar complejo deportivo",
+                prefixIcon: Icon(Icons.search, color: Colors.grey),
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          // Ya no se muestra la fila de categorías
+          // Grid de productos (se muestran todos los ítems)
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 0.8,
+                children: allItems.map((item) {
+                  return ItemCard(
+                    image: item.image,
+                    name: item.name,
+                    price: item.price,
+                    location: item.location,
+                    rating: item.rating,
+                    description: item.description,
+                    owner: item.owner,
+                    email: item.email,
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Modelo para cada ítem (complejo deportivo)
+class Item {
+  final String image;
+  final String name;
+  final double price;
+  final String location;
+  final double rating;
+  final String description;
+  final String owner;
+  final String email;
+
+  Item({
+    required this.image,
+    required this.name,
+    required this.price,
+    required this.location,
+    required this.rating,
+    required this.description,
+    required this.owner,
+    required this.email,
+  });
+}
+
+// Tarjeta para mostrar cada ítem en el grid
+class ItemCard extends StatelessWidget {
+  final String name;
+  final String image;
+  final double price;
+  final String location;
+  final double rating;
+  final String description;
+  final String owner;
+  final String email;
+
+  const ItemCard({
+    required this.name,
+    required this.image,
+    required this.price,
+    required this.location,
+    required this.rating,
+    required this.description,
+    required this.owner,
+    required this.email,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.black, // Fondo negro para contraste
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15)),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              name,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Colors.white),
+            ),
+            SizedBox(height: 8),
+            Image.asset(image, height: 80),
+            SizedBox(height: 8),
+            Text(
+              "Rp $price",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            IconButton(
+              icon: Icon(Icons.add, color: Colors.white),
+              onPressed: () {
+                // Navegar a la página de detalles pasando los datos del ítem
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ItemDetailPage(
+                      name: "Cancha de Soccer 1",
+                      price: 100000,
+                      location: "Ubicación 1",
+                      rating: 4.5,
+                      description: "Cancha de soccer con césped natural.",
+                      owner: "Carlos",
+                      email: "carlos@example.com",
+                      image: 'assets/images/cancha01.jpg', // Nueva propiedad
+                    ),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
